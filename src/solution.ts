928140408bb14c9e19aca2249777162fec184ce5
srshakil2 <rh3642921@gmail.com>
2025-11-17 17:29:19 +0600

    added problem 8 solution and chaking all problem solution

--- src/solution.ts
+++ src/solution.ts
@@ -8,9 +8,9 @@ const formatValue = (value: InputValue): InputValue => {
     return !value;
   }
 };
-// console.log(formatValue("Hello"));
-// console.log(formatValue(5));
-// console.log(formatValue(false));
+console.log(formatValue("Hello"));
+console.log(formatValue(5));
+console.log(formatValue(true));
 
 type InputLength = string | number[];
 const getLength = (value: InputLength): number => {
@@ -21,8 +21,8 @@ const getLength = (value: InputLength): number => {
   }
   throw new Error("Please provide a valid input");
 };
-// console.log(getLength("TypeScript"));
-// console.log(getLength([1, 2, 3, 4, 5]));
+console.log(getLength("typescript"));
+console.log(getLength([10, 20, 30, 40]));
 
 class Person {
   name: string;
@@ -32,37 +32,37 @@ class Person {
     this.age = age;
   }
   getDetails(): string {
-    return `Name: ${this.name}, Age: ${this.age}`;
+    return `'Name: ${this.name}, Age: ${this.age}'`;
   }
 }
-// const person1 = new Person("John Doe", 30);
-// console.log(person1.getDetails());
-// const person2 = new Person("Alice", 25);
-// console.log(person2.getDetails());
+const person1 = new Person("John Doe", 30);
+const person2 = new Person("Alice", 25);
+console.log(person1.getDetails());
+console.log(person2.getDetails());
 
 type Item = { title: string; rating: number };
 const filterByRating = (items: Item[]): Item[] => {
   const filteredItems = items.filter((item) => item.rating >= 4);
   return filteredItems;
 };
-// const books = [
-//   { title: "Book A", rating: 4.5 },
-//   { title: "Book B", rating: 3.2 },
-//   { title: "Book C", rating: 5.0 },
-// ];
-// console.log(filterByRating(books));
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+console.log(filterByRating(books));
 
 type User = { id: number; name: string; email: string; isActive: boolean };
 const filterActiveUsers = (users: User[]): User[] => {
   const filteredUsers = users.filter((user) => user.isActive);
   return filteredUsers;
 };
-// const users = [
-//   { id: 1, name: "Rakib", email: "rakib@example.com", isActive: true },
-//   { id: 2, name: "Asha", email: "asha@example.com", isActive: false },
-//   { id: 3, name: "Rumi", email: "rumi@example.com", isActive: true },
-// ];
-// console.log(filterActiveUsers(users));
+const users = [
+  { id: 1, name: "Rakib", email: "rakib@example.com", isActive: true },
+  { id: 2, name: "Asha", email: "asha@example.com", isActive: false },
+  { id: 3, name: "Rumi", email: "rumi@example.com", isActive: true },
+];
+console.log(filterActiveUsers(users));
 
 interface Book {
   title: string;
@@ -70,19 +70,19 @@ interface Book {
   publishedYear: number;
   isAvailable: boolean;
 }
-const printBookDetails = (book: Book) => {
+const printBookDetails = (book: Book): void => {
   const availability = book.isAvailable ? "Yes" : "No";
   return console.log(
     `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${availability}`
   );
 };
-// const myBook: Book = {
-//   title: "The Great Gatsby",
-//   author: "F. Scott Fitzgerald",
-//   publishedYear: 1925,
-//   isAvailable: true,
-// };
-// printBookDetails(myBook);
+const myBook: Book = {
+  title: "The Great Gatsby",
+  author: "F. Scott Fitzgerald",
+  publishedYear: 1925,
+  isAvailable: true,
+};
+printBookDetails(myBook);
 
 type ArrayType = number[] | string[];
 const getUniqueValues = (arr1: ArrayType, arr2: ArrayType) => {
@@ -113,6 +113,28 @@ const getUniqueValues = (arr1: ArrayType, arr2: ArrayType) => {
   }
   return unique;
 };
-// const array1: number[] = [1, 2, 3, 4, 5];
-// const array2: number[] = [3, 4, 5, 6, 7];
-// console.log(getUniqueValues(array1, array2));
+const array1: number[] = [1, 2, 3, 4, 5];
+const array2: number[] = [3, 4, 5, 6, 7];
+console.log(getUniqueValues(array1, array2));
+
+interface Product {
+  name: string;
+  price: number;
+  quantity: number;
+  discount?: number;
+}
+const calculateTotalPrice = (products: Product[]): number => {
+  const total = products.reduce((acc, product) => {
+    const discount = product?.discount ? product.discount : 0;
+    const productTotal =
+      product.price * product.quantity * (1 - discount / 100);
+    return acc + productTotal;
+  }, 0);
+  return total;
+};
+const products = [
+  { name: "Pen", price: 10, quantity: 2 },
+  { name: "Notebook", price: 25, quantity: 3, discount: 10 },
+  { name: "Bag", price: 50, quantity: 1, discount: 20 },
+];
+console.log(calculateTotalPrice(products));
